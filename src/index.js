bda53fc870b126a9f637e6bbd53626a6ee54d47b
Vaibhav Tandon <vaibhav.tandongcet@gmail.com>
2021-04-03 22:19:41 +0530

    Added icon

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,3 +12,4 @@ ReactDOM.render(
 );
 
 
+serviceWorker.register();
